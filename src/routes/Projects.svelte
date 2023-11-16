5842c2673198387e347dff7a3eb7cb0b64293fb9
Caleb Filburn <cfilburn@isaca.org>
2023-11-16 00:34:26 -0500

    Add projects component

---
+++ src/routes/Projects.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let img:string;
+  export let alt:string;
+  export let title:string;
+  export let description:string;
+  export let tech:string[];
+</script>
+
+<li>
+  <img src="{img}" alt="{alt}" />
+  <div>
+    <h3>{title}</h3>
+    {#if description}
+      <p>{description}</p>
+    {/if}
+    <ul class="tech">
+      {#each tech as t}
+        <li>{t}</li>
+      {/each}
+    </ul>
+  </div>
+</li>
