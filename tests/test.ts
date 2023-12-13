71e14d7c7f82ef90908b53c30a4540c76b778171
Caleb Filburn <calebfilburn@gmail.com>
2023-12-13 17:56:55 -0500

    Add some basic tests

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,18 @@ test('page has title', async ({ page }) => {
 	await page.goto('/');
 	await expect(page).toHaveTitle(/Caleb Filburn/);
 });
+
+test('main heading exists', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'Caleb Filburn' })).toBeVisible();
+});
+
+test('resume link', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('link', { name: 'See full resume' })).toBeVisible();
+});
+
+test('experience heading exists', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'Experience' })).toHaveClass(/sr-only/);
+});
